1f64a77fee086a9f922081832f6a70b7edfca7a6
robert <robert@masikh.org>
2023-12-08 15:26:32 +0100

    Enforce correct attribute type for used task model

--- models/task_model.py
+++ models/task_model.py
@@ -3,6 +3,46 @@ from datetime import datetime, timedelta
 from enum import Enum
 
 
+def enforce_types(cls):
+    """ Decorator method. Its purpose is to enforce the types for class attributes
+        used upon initialization and modification.
+    """
+
+    class Wrapper(cls):
+        """ This wrapper class overrides the __init__ and __setattr__ of the wrapped
+            class in order to check if the type of its attributes adhere to its annotation
+        """
+
+        def __init__(self, *args, **kwargs):
+            """ Override the 'dunder' __init__ in order to check for correct types upon
+                initialization.
+            """
+            super().__init__(*args, **kwargs)
+            self._check_types()
+
+        def __setattr__(self, name, value):
+            """ Override the 'dunder' __setattr__ in order to check for correct types upon
+                modification.
+            """
+            super().__setattr__(name, value)
+            self._check_types()
+
+        def _check_types(self):
+            """ Check each class attribute using its annotation for correct types """
+
+            # Get all attributes and its values of the class
+            for attribute_name, attribute_type in self.__annotations__.items():
+                # get the value for the attribute 'field_name'
+                attribute_value = getattr(self, attribute_name)
+
+                # if the field its value is not of the type (annotation) for this field, raise an exception
+                if not isinstance(attribute_value, attribute_type):
+                    raise TypeError(f"Invalid type for '{attribute_name}': {type(attribute_value).__name__}")
+
+    # Return the wrapped class with the overridden 'dunders'.
+    return Wrapper
+
+
 class TaskStatus(Enum):
     """
     Enum class for the status of a task.
@@ -16,6 +56,7 @@ class TaskStatus(Enum):
     # DISCARDED = "discarded"
 
 
+@enforce_types
 @dataclass
 class Task:
     """
