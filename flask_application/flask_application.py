fd146ea80aa14ef7b2a9356c7e820bc8e3026676
robert <robert@masikh.org>
2023-12-09 14:37:59 +0100

    Minor refactoring and comments

--- flask_application/flask_application.py
+++ flask_application/flask_application.py
@@ -18,10 +18,12 @@ class APIServer:
         self.port = port
         self.debug = bool(os.getenv("debug") is not None)
         self.app = Flask(f'Flask Server running on port: {self.port}')
-        self.server = None
+        self.wsgi_server = None
 
     def config(self):
         """ Setup Flask application defaults """
+
+        # Configuration parameters for tuning Flasks behaviour to our needs
         self.app.config['JSON_SORT_KEYS'] = True
         self.app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', str(uuid4()))
         self.app.config['SESSION_COOKIE_HTTPONLY'] = True
@@ -38,26 +40,38 @@ class APIServer:
 
     def create_tables(self):
         """ Create database tables """
-        with self.app.app_context():
+
+        # If the tables already exist it will silently continue
+        with self.app.app_context():  # <- Is this really needed?
             db.create_all()
 
     def run(self):
         """ Start API server"""
+
+        # Friendly CLI message
         print(f'API: http://{self.ip}:{self.port}')
+
+        # Setup Flask configuration parameters
         self.config()
+
+        # Register blueprints (routes)
         self.app.register_blueprint(api)
 
         # Create database
         self.create_tables()
 
-        self.server = WSGIServer(self.app, host=self.ip, port=self.port)
-        self.server.start()
+        # Start the Flask application
+        self.wsgi_server = WSGIServer(self.app, host=self.ip, port=self.port)
+        self.wsgi_server.start()
 
     def stop(self):
         """ Stop API server """
+
+        # server.stop() is wrapped in a try except. Reasoning is that WSGI is not up to par with is_alive
+        # thread. It still uses the obsoleted isAlive(). Since the server is being stopped this error is
+        # annoying but not 'fatal'.
         try:
-            self.server.stop()
+            self.wsgi_server.stop()
         except AttributeError:
-            if self.debug:
-                print("'WorkerThread' object has no attribute 'isAlive'")
-                print("NOTE: Your WSGI doesn't support the is_alive thread methods! (python >3.8)")
+            print("'WorkerThread' object has no attribute 'isAlive'")
+            print("NOTE: Your WSGI doesn't support the is_alive thread methods! (python >3.8)")
