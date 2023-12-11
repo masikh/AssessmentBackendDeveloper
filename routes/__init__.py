83f68572f744d3ef76761aa8ac961d7a28987d0e
robert <robert@masikh.org>
2023-12-11 17:37:06 +0100

    black (magic... ;)

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,16 +1,24 @@
 """ __init__ for routes """
 
 from flask import Blueprint
-doc = Blueprint('doc', __name__)
-api = Blueprint('api', __name__)
-auth = Blueprint('auth', __name__)
+
+doc = Blueprint("doc", __name__)
+api = Blueprint("api", __name__)
+auth = Blueprint("auth", __name__)
 
 # DOC
 from routes.render_readme import render_readme  # pylint: disable=wrong-import-position
 
 # API
-from routes.api_crud_task import api_crud_task  # pylint: disable=wrong-import-position
-from routes.api_search_task import api_search_task  # pylint: disable=wrong-import-position
+from routes.api_crud_task import (
+    APITask,
+)  # pylint: disable=wrong-import-position
+from routes.api_search_task import (
+    api_search_task,
+)  # pylint: disable=wrong-import-position
 
 # Auth
-from routes.api_authorization import api_user_create, api_user_login  # pylint: disable=wrong-import-position
+from routes.api_authorization import (
+    api_user_create,
+    api_user_login,
+)  # pylint: disable=wrong-import-position
