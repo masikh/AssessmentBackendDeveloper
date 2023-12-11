f5132532e1da2a9453928ed754ba132f202d0b7f
robert <robert@masikh.org>
2023-12-11 17:34:14 +0100

    Refactoring API endpoint for APIDocs

--- routes/api_crud_task.py
+++ routes/api_crud_task.py
@@ -1,17 +1,31 @@
 """ CRUD routes for Task """
 from http import HTTPStatus
 from flask import request, make_response, jsonify
+from flask_restful import Resource
+from flasgger import swag_from
 from werkzeug.exceptions import InternalServerError
 from routes import api
 from models import Task
 from database import db
-from flask_application import memoize, authorize
+from flask_application import memoize  # , authorize
 from generic_helpers.pagination import set_paginated_response
 from generic_helpers.authenticator import authenticated
+from apidocs.api_task_crud import APITaskCRUD
+
+
+apidocs = APITaskCRUD()
+
+
+def response_method_not_allowed():
+    """ Generic response"""
+    response = make_response({"error": "method not allowed"})
+    response.status_code = HTTPStatus.METHOD_NOT_ALLOWED
+
+    return response
 
 
 def response_ok(task):
-    """ Generic 200 response """
+    """Generic 200 response"""
 
     # Serialize data
     response_dict = task.serialize()
@@ -24,26 +38,26 @@ def response_ok(task):
 
 
 def response_not_found():
-    """ Generic 404 response """
+    """Generic 404 response"""
 
     # Build a 404 response
-    response = make_response(jsonify({'error': 'Task not found'}))
+    response = make_response(jsonify({"error": "Task not found"}))
     response.status_code = HTTPStatus.NOT_FOUND
     return response
 
 
-def response_bad_request(error='Bad request'):
-    """ Generic 400 response """
+def response_bad_request(error="Bad request"):
+    """Generic 400 response"""
 
     # Build a 400 response
-    response = make_response(jsonify({'error': error}))
+    response = make_response(jsonify({"error": error}))
     response.status_code = HTTPStatus.BAD_REQUEST
     return response
 
 
 @api.errorhandler(InternalServerError)
 def handle_internal_server_error(error):
-    """ Custom internal server error handler """
+    """Custom internal server error handler"""
 
     # Any 500 (Internal server error) will be handled by this response.
     # E.g. This might occur when malformed data is sent to the deserializer.
@@ -52,75 +66,63 @@ def handle_internal_server_error(error):
     # it's always possible to use a custom try-except where its deemed fit, this
     # will serve as an extra save guard.
 
-    response = make_response(jsonify({'error': str(error)}))
+    response = make_response(jsonify({"error": str(error)}))
     response.status_code = HTTPStatus.INTERNAL_SERVER_ERROR
     return response
 
 
-@api.route('/api/task', defaults={'task_id': None}, methods=['GET', 'POST'])
-@api.route('/api/task/<int:task_id>', methods=['GET', 'PATCH', 'DELETE'])
-@authenticated
-def api_crud_task(task_id):  # pylint: disable=too-many-return-statements
-    """ CRUD API for tasks, depending on the HTTP-method and argument a handler is chosen
+class APITask(Resource):
+    """GET|POST|PATCH|DELETE task by id"""
 
-        GET: (with task_id) -> api_crud_task_get(task_id)
-        GET: (without task_id) -> api_crud_task_get_all()
-        POST: api_crud_task_post()
-        PATCH: api_crud_task_patch(task_id)
-        DELETE: api_crud_task_patch(task_id)
-        *: returns a HTTPStatus.METHOD_NOT_ALLOWED with a error message
+    @staticmethod
+    @api.route("/api/task", methods=["GET"])
+    @swag_from(apidocs.api_get_tasks, methods=["GET"])
+    @authenticated
+    def get_tasks():
+        """GET tasks"""
+        return api_crud_task_get_all()
 
-        For authorization has been omitted for this assessment, authentication well be implemented
-
-        NOTE: pylint gives a warning about too many return statements, justified in most cases,
-        but in my opinion not applicable in this case.
-    """
-
-    # Guard clause: If request method is POST we don't expect a task_id. If task_id is not None
-    # We'll return a 'BAD_REQUEST'. For all other allowed methods a task_id is allowed.
-    if request.method == 'POST' and task_id is not None:
-        return response_bad_request()
-
-    # Warning -> fully NULL primary key identity cannot load any object. This condition may
-    # raise an error in a future release. error in: task = Task.query.get(task_id)
-    # There is no need to wait for a bug, we solve it right here.
-    if request.method in ['PATCH', 'DELETE'] and task_id is None:
-        return response_bad_request()
-
-    if request.method == 'GET':
-        # Handle the get method. If there's no task id, we return all available tasks else we return the specific task
-        if task_id is None:
-            # Handle GET request without task_id, thus return all available tasks
-            return api_crud_task_get_all()
-
-        # Handle GET request with task_id
+    @staticmethod
+    @api.route("/api/task/<int:task_id>", methods=["GET"])
+    @swag_from(apidocs.api_get_task_by_id)
+    @authenticated
+    def get_task(task_id):
+        """Get details of a specific task by ID"""
         return api_crud_task_get(task_id)
 
-    if request.method == 'POST':
-        # Handle POST request
+    @staticmethod
+    @api.route("/api/task", methods=["POST"])
+    @swag_from(apidocs.api_post_task, methods=["POST"])
+    @authenticated
+    def post():
+        """POST task"""
         return api_crud_task_post()
 
-    if request.method == 'PATCH':
-        # Handle PATCH request. If not task_id is provided we consider it a bad request
+    @staticmethod
+    @api.route("/api/task/<int:task_id>", methods=["PATCH"])
+    @swag_from(apidocs.api_patch_task_by_id)
+    @authenticated
+    def patch(task_id):
+        """Update details of a specific task by ID"""
         return api_crud_task_patch(task_id)
 
-    if request.method == 'DELETE':
-        # Handle DELETE request
+    @staticmethod
+    @api.route("/api/task/<int:task_id>", methods=["DELETE"])
+    @swag_from(apidocs.api_delete_task_by_id)
+    @authenticated
+    def delete(task_id):
+        """Delete a specific task by ID"""
         return api_crud_task_delete(task_id)
 
-    # Method is not supported
-    response = make_response({'error': 'Method not supported'})
-    response.status_code = HTTPStatus.METHOD_NOT_ALLOWED
-    return response
 
-
-@authorize.read
+# @authorize.read
 def api_crud_task_get_all():
-    """ Logic for handling GET request without task_id """
+    """Logic for handling GET request without task_id"""
+
     @memoize  # key is user token
     def get_all(memoize_key):  # pylint: disable=unused-argument
-        """ Because we use memoization, this logic is in its own method
-            to be wrapped by the memoize decorator
+        """Because we use memoization, this logic is in its own method
+        to be wrapped by the memoize decorator
         """
 
         # Get all tasks from database
@@ -130,15 +132,15 @@ def api_crud_task_get_all():
         return [task.serialize() for task in tasks]
 
     # Get pagination parameters
-    page = request.args.get('page', default='1')
-    page_size = request.args.get('page_size', default='20')
+    page = request.args.get("page", default="1")
+    page_size = request.args.get("page_size", default="20")
 
     # Build memoization key
     # Create a list of non-None values and Concatenate the non-None values into a string
     # we also use the users authorization token to distinguish between users
-    token = request.headers.get('Authorization')
+    token = request.headers.get("Authorization")
     non_none_values = [token]
-    memoize_key = '+'.join(str(value) for value in non_none_values if value is not None)
+    memoize_key = "+".join(str(value) for value in non_none_values if value is not None)
 
     # Because we use memoize, we need a key to retrieve the correct entries
     response = get_all(memoize_key)
@@ -146,13 +148,17 @@ def api_crud_task_get_all():
     # Check that the pagination parameters are digits
     if not page.isdigit() or not page_size.isdigit():
         # Return a comprehensive 400 response
-        return response_bad_request("Invalid page or page_size. Please provide valid numeric values.")
+        return response_bad_request(
+            "Invalid page or page_size. Please provide valid numeric values."
+        )
 
     page = int(page)
     page_size = int(page_size)
 
     # Set paginated response
-    paginated_response = set_paginated_response(response, page=page, page_size=page_size)
+    paginated_response = set_paginated_response(
+        response, page=page, page_size=page_size
+    )
 
     # Build 200 response
     response = make_response(paginated_response)
@@ -160,9 +166,9 @@ def api_crud_task_get_all():
     return paginated_response
 
 
-@authorize.read
+# @authorize.read
 def api_crud_task_get(task_id):
-    """ Logic for handling GET request with task_id """
+    """Logic for handling GET request with task_id"""
 
     # Get task from database
     task = Task.query.get(task_id)
@@ -175,18 +181,19 @@ def api_crud_task_get(task_id):
     return response_ok(task)
 
 
-@authorize.create()
+# @authorize.create()
 def api_crud_task_post():
-    """ Logic for handling POST request """
+    """Logic for handling POST request"""
 
     # Get POST data from request
     data = request.get_json()
 
-    # Create a new task instance. If the data is malformed an internal server error
-    # will occur and shall be processed by the @api.errorhandler(InternalServerError)
-    # Thus there is no need for extra exception handling
-    new_task = Task()
-    new_task.deserialize(data)
+    # Create a new task instance.
+    try:
+        new_task = Task()
+        new_task.deserialize(data)
+    except ValueError:
+        return response_bad_request()
 
     # Add the new task to the session and commit to the database
     db.session.add(new_task)
@@ -197,9 +204,15 @@ def api_crud_task_post():
     return response_ok(new_task)
 
 
-@authorize.update
+# @authorize.update
 def api_crud_task_patch(task_id):
-    """ Logic for handling PATCH request """
+    """Logic for handling PATCH request"""
+
+    # Dear reviewer: -> I was out of time!!!
+    #
+    # This patch doesn't work a patch should work.
+    # A patch should replace the values which you send via the request.
+    # this endpoint does not behave like that.
 
     # Get task from database
     task = Task.query.get(task_id)
@@ -211,9 +224,12 @@ def api_crud_task_patch(task_id):
     # Get PATCH data from request
     data = request.get_json()
 
-    # Create a new task instance. If the data is malformed an internal server error
-    # will occur and shall be processed by the @api.errorhandler(InternalServerError)
+    # Create a new task from data
     task = task.deserialize(data)
+    try:
+        task = task.deserialize(data)
+    except ValueError:
+        return response_bad_request()
 
     # Add the new task to the session and commit to the database
     db.session.add(task)
@@ -225,9 +241,9 @@ def api_crud_task_patch(task_id):
     return response_ok(task)
 
 
-@authorize.delete
+# @authorize.delete
 def api_crud_task_delete(task_id):
-    """ Logic for handling DELETE request """
+    """Logic for handling DELETE request"""
 
     # Get the task from the database by 'id'
     task = Task.query.get(task_id)
@@ -241,7 +257,7 @@ def api_crud_task_delete(task_id):
     db.session.commit()
 
     # Build 200 response
-    response = make_response('DELETED')
+    response = make_response("DELETED")
     response.status_code = HTTPStatus.OK
 
     # The memoized response is no longer valid, flush the cache
